3d0a635061632ec076bd679bc9bc5ac21c0de5e0
wasiushisir <wasiuahmed410@gmail.com>
2023-08-06 14:45:34 +0600

    implementing global error handler

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userRoute = require("../src/app/modules/user/user.route");
 const noteRoute = require("../src/app/modules/note/note.route.js");
+const { errorHandler } = require("../src/middleware/errormiddleware");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
@@ -9,5 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users/", userRoute);
 app.use("/api/note/", noteRoute);
+app.use(errorHandler);
 
 module.exports = app;
